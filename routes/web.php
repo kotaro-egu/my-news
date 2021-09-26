a079df9cadd31fd9cca7be486b951ac15885045a
EC2 Default User <ec2-user@ip-172-31-8-211.us-east-2.compute.internal>
2021-09-26 08:31:55 +0000

    19sixyou

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
+Route::get('/',           'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
